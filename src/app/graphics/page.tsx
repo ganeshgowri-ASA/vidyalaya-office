f4b40365453f893c153b515b6c8849bf0d40779f
ASA <ganeshgowri.mitsui@gmail.com>
2026-03-16 17:55:38 +0530

    Fix: use default import for GraphicsEditor in page.tsx

--- src/app/graphics/page.tsx
+++ src/app/graphics/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { GraphicsEditor } from '@/components/graphics/graphics-editor';
+import GraphicsEditor from '@/components/graphics/graphics-editor';
 
 export default function GraphicsPage() {
   return <GraphicsEditor />;
